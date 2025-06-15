15881085a0018fdcd04ac4416ca2d136025967e2
Andrew Brown <devandrewbro@gmail.com>
2025-06-15 08:08:40 -0400

    added insert and remove method

--- doubleLinkedList.js
+++ doubleLinkedList.js
@@ -34,12 +34,37 @@ class DoubleLinkedList {
     this.length++;
     return this;
   }
-  // insert(index, value) {
+  insert(index, value) {
+    if (index == 0) {
+      this.prepend(value);
+      return this;
+    }
+    let newNode = new Node(value);
+    let leftNode = this.traverseToIndex(index - 1);
+    let rightNode = leftNode.next;
+    leftNode.next = newNode;
+    newNode.previous = leftNode;
+    newNode.next = rightNode;
+    rightNode.previous = newNode;
+    this.length++;
+    return this;
+  }
 
-  // }
-  // remove(index) {
+  remove(index) {
+    if (index == 0) {
+      this.head = this.head.next;
+      this.head.previous = null;
+    }
+    if (index != 0) {
+      const beforeTargetNode = this.traverseToIndex(index - 1);
+      const targetNode = this.traverseToIndex(index);
+      beforeTargetNode.next = targetNode.next;
+      beforeTargetNode.next.previous = beforeTargetNode;
+    }
+    this.length--;
+    return this;
+  }
 
-  // }
   printList() {
     let array = [];
     let count = 0;
@@ -51,12 +76,21 @@ class DoubleLinkedList {
     }
     console.log(array);
   }
-  // traverseToIndex(index) {
-
-  // }
+  traverseToIndex(index) {
+    let nodeHolder = this.head;
+    let count = 0;
+    while (count < index) {
+      nodeHolder = nodeHolder.next;
+      count++;
+    }
+    return nodeHolder;
+  }
 }
 
 let myDoubleLinkedList = new DoubleLinkedList(50);
 myDoubleLinkedList.append(5);
 myDoubleLinkedList.append(7);
+myDoubleLinkedList.prepend(10);
+myDoubleLinkedList.insert(3, 2);
+myDoubleLinkedList.remove(0);
 myDoubleLinkedList.printList();
