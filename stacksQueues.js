695a29ec99b667e08362ab6b142b9fa146b99da0
Andrew Brown <devandrewbro@gmail.com>
2025-06-17 17:35:25 -0400

    new file about stacks and queues. documentations that explains them.

---
+++ stacksQueues.js
@@ -0,0 +1,24 @@
+//Stacks is a type of data structure the resembiles stacking plates. (like kitchen plates)
+//they are stacked on top of each other and you can only access the top plate.
+//LIFO. last in first out.
+//they are very good when you need the last value and is good in language efficient engines
+//browser history is a stack. an undo button's idea comes from a stack.
+//pop O(1), push O(1), peek O(1), lookup O(n)
+
+//Queues are like a line of people waiting to eat.
+//the first person to arrive is the first person to go.
+//FIFO first in first out.
+//queues are used alot in programming problems and interview questions.
+//resturant app, uber, lift, a printer. all use Queues
+//lookup O(n), enqueue O(1), dequeue O(1), peek O(1)
+
+//why would you not wanna use an array to build a queue?
+//I would say it is because when you remove from the front of an array
+//everything has to shift forward and will take more time o(n) I believe.
+
+//JS does not have its own stack or queues data structure.
+//we have to make it.
+
+//Stacks
+//we vist google, udemy, youtube. in that order on a browser
+//we can build it with arrays or linked lists
